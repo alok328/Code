ccfdf2fcd65251621fbf6bf265743373d95ae389
alok328 <alok328raj@gmail.com>
2021-04-17 13:10:37 +0530

    Removed extras

--- insertionSort.java
+++ insertionSort.java
@@ -1,12 +1,9 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
+class InsertionSort {
 	
-	public static void insertionSort(int[] a){
+	public static void insertionSort(int[] a) {
 		for(int j=1; j<a.length; j++){
 			int cur = a[j];
 			int i = j-1;
@@ -18,8 +15,7 @@ class Ideone
 		}
 	}
 	
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	public static void main (String[] args){
 		int[] a = {5, 2, 4, 3, 1, 6};
 		insertionSort(a);
 		for(int x : a){
